7243cc7318fbd7ca7c91c8199f559bdb3216ce72
Artem Polishchuk <art4315@gmail.com>
2020-09-26 21:58:54 +0300

    Car class creation task

---
+++ src/org/lab/Car.java
@@ -0,0 +1,70 @@
+package org.lab;
+
+public class Car {
+    private int id;
+    private String brand;
+    private int productionYear;
+    private double price;
+    private String color;
+    private int registrationNumber;
+
+    public Car() {
+    }
+
+    public Car(int id, String brand, int productionYear, double price, String color, int registrationNumber) {
+        this.id = id;
+        this.brand = brand;
+        this.productionYear = productionYear;
+        this.price = price;
+        this.color = color;
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(int registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+}
