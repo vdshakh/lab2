a57100da9f22580bb459a1faab0ee090b897737f
vdshakh <vdshakh@gmail.com>
2020-10-03 11:13:55 +0300

    practice

--- src/org/lab/CarFilter.java
+++ src/org/lab/CarFilter.java
@@ -3,24 +3,37 @@ package org.lab;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CarFilter {
-    public static List<Car> filterByBrand(List<Car> cars, String brand){
+    /*public static List<Car> filterByBrand(List<Car> cars, String brand){
         List<Car> result = new ArrayList<>();
         for(Car vehicle: cars){
             if(vehicle.getBrand().equalsIgnoreCase(brand))
                 result.add(vehicle);
         }
         return result;
+    }*/
+
+    public static List<Car> filterByBrand(List<Car> cars, String brand){
+        return cars.stream() //list.stream()
+                .filter(car -> (car.getBrand().equalsIgnoreCase(brand))) //filter(Predicate predicate)
+                .collect(Collectors.toList()); //collect(Collector collector)
     }
 
-    public static List<Car> filterByBrandAndExploitationDuration(List<Car> cars, String brand, int year){
+    /*public static List<Car> filterByBrandAndExploitationDuration(List<Car> cars, String brand, int year){
         List<Car> result = new ArrayList<>();
         for(Car vehicle: cars){
             if((2020 - vehicle.getProductionYear() > year) && vehicle.getBrand().equalsIgnoreCase(brand))
                 result.add(vehicle);
         }
         return result;
+    }*/
+
+    public static List<Car> filterByBrandAndExploitationDuration(List<Car> cars, String brand, int year){
+        return cars.stream() //list.stream()
+                .filter(car -> (car.getProductionYear() > year) && car.getBrand().equalsIgnoreCase(brand)) //filter(Predicate predicate)
+                .collect(Collectors.toList()); //collect(Collector collector)
     }
 
     public static List<Car> filterByProductionYearAndPrice(List<Car> cars, int year, double price){
